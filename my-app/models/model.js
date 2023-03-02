42d2ede661c42696a8a6d1dc5edb9216fde3ac3f
Vesilya Gezer <vesilya.gezer@ozu.edu.tr>
2023-03-02 12:50:50 +0300

    hello git

--- my-app/models/model.js
+++ my-app/models/model.js
@@ -2,8 +2,31 @@ const mongoose = require("../database");
  
 // create an schema
 var userSchema = new mongoose.Schema({
-            name: String,
-            email:String
+    _id: String,
+    listing_url: String,
+    name: String,
+    summary: String,
+    space: String,
+    description: String,
+    neighborhood_overview: String,
+    notes: String,
+    transit: String,
+    access: String,
+    interaction: String,
+    house_rules: String,
+    property_type: String,
+    room_type: String,
+    bed_type: String,
+    minimum_nights: String,
+    maximum_nights: String,
+    cancellation_policy: String,
+    last_scraped: Mixed,
+    calendar_last_scraped:Mixed,
+    first_review:Mixed ,
+    last_review: Mixed,
+    accommodates: Number,
+    bedrooms: Number,
+    beds: Number,
         });
  
 var userModel=mongoose.model('users',userSchema);
