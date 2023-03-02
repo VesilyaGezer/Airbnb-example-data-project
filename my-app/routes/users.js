399b32d4b53b4d88cc37ae720362699d536df0c3
Vesilya Gezer <vesilya.gezer@ozu.edu.tr>
2023-03-02 12:06:21 +0300

    hello git

--- my-app/routes/users.js
+++ my-app/routes/users.js
@@ -1,9 +1,21 @@
 var express = require('express');
 var router = express.Router();
-
-/* GET users listing. */
+var mongoose = require('mongoose');
+var userModel = require('../models/userModel');
+ 
+ 
+/* GET home page. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+      
+    userModel.find((err, docs) => {
+        if (!err) {
+            res.render("list", {
+                data: docs
+            });
+        } else {
+            console.log('Failed to retrieve the Course List: ' + err);
+        }
+    });
+ 
 });
-
 module.exports = router;
